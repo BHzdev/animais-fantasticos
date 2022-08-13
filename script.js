283ae36af2cd6ea5042a8785d5670aec7fcebbd3
BrHz <bruno_tcc_tc@hotmail.com>
2022-08-13 15:11:10 -0300

    add faq accordion list

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function initTabNav() {
   const tabMenu = document.querySelectorAll(".js-tabmenu li");
   const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const activeClass = "ativo";
 
   if (tabMenu.length && tabContent.length) {
     tabContent[0].classList.add("ativo");
@@ -21,3 +22,24 @@ function initTabNav() {
 }
 
 initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      console.log(this.nextElementSibling.classList.toggle(activeClass));
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
